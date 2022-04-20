0e4ab0d7f1bf5a5ecade916510d76567781043bc
Maya Van Dijck <r0758052@pc-klas2-6.esat.kuleuven.be>
2022-04-20 12:42:14 +0200

    RTL map changed

--- processor_design_group1/MULT3/RTL/hazard_unit.v
+++ processor_design_group1/MULT3/RTL/hazard_unit.v
@@ -1,23 +1,23 @@
 
 module hazard_unit(
-      input reg [31:0] instruction_ID,
-      input reg [31:0] instruction_EXE,
-      input reg	       MemRead_EXE,
-      input reg	       MemRead_ID,
+      input wire [31:0] instruction_ID,
+      input wire [31:0] instruction_EXE,
+      input wire	       MemRead_EXE,
+      input wire	       MemRead_ID,
       output reg       PCWrite,
       output reg       IF_ID_Write,
       output reg       stall_sel
    );
 
 always @(*) begin
-	assign stall_sel = 1'b1;
-	assign IF_ID_Write = 1'b1;
-	assign PCWrite = 1'b1;	
+	stall_sel = 1'b1;
+	IF_ID_Write = 1'b1;
+	PCWrite = 1'b1;	
 	
 	if (MemRead_EXE == 1'b1 && MemRead_ID == 1'b0 && ((instruction_EXE[11:7] == instruction_ID[19:15]) || (instruction_EXE[11:7] == instruction_ID[24:20]))) begin
-		assign stall_sel = 1'b0;
-		assign IF_ID_Write = 1'b0;
-		assign PCWrite = 1'b0;
+		stall_sel = 1'b0;
+		IF_ID_Write = 1'b0;
+		PCWrite = 1'b0;
 	end 
 end
 
